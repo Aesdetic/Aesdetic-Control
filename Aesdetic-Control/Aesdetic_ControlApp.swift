d2e0869723664fe4604bbb615506978e5bfba523
Ryan Tam <support@aesdetic.com>
2025-06-26 15:23:49 +0800

    Initial Commit

---
+++ Aesdetic-Control/Aesdetic_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aesdetic_ControlApp.swift
+//  Aesdetic-Control
+//
+//  Created by Ryan Tam on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Aesdetic_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aesdetic-ControlTests/Aesdetic_ControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  Aesdetic_ControlTests.swift
+//  Aesdetic-ControlTests
+//
+//  Created by Ryan Tam on 6/26/25.
+//
+
+import Testing
+@testable import Aesdetic_Control
+
+struct Aesdetic_ControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
